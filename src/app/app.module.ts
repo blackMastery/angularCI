fabae8a0839e4cfe4b76e638acc58ad4cabee812
blackMastery <black.king1232@gmail.com>
2019-08-15 13:50:11 -0400

    first

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
 
 import { AppComponent } from './app.component';
+import { CrisisListComponent } from './crisis-list/crisis-list.component';
+import { HeroListComponent } from './hero-list/hero-list.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { AppRoutingModule } from './app-routing.module';
+import { HeroesModule } from './heroes/heroes.module';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CrisisListComponent,
+    PageNotFoundComponent,
+ 
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+   HeroesModule,
+   AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
